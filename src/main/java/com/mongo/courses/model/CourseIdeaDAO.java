6cf45635a66cfc7b9d31fe026719232086803e02
Loop421 <lopez.e421@gmail.com>
2018-03-23 00:32:35 -0400

    Initial commit

---
+++ src/main/java/com/mongo/courses/model/CourseIdeaDAO.java
@@ -0,0 +1,11 @@
+package com.mongo.courses.model;
+
+import java.util.List;
+
+public interface CourseIdeaDAO {
+    boolean add(CourseIdea idea);
+
+    List<CourseIdea> findAll();
+
+    CourseIdea findBySlug(String slug);
+}
---
+++ src/main/java/com/mongo/courses/model/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.mongo.courses.model;
+
+public class NotFoundException extends  RuntimeException{
+}
